79f4f390a61eb94a54e1534bae8a7afdfdc5c26d
bradlet <bradlet2@pdx.edu>
2022-06-15 08:26:31 -0700

    Use near-contract-standards core impls instead of starting my own

--- src/lib.rs
+++ src/lib.rs
@@ -3,31 +3,70 @@
 /// @author bradlet <bradlet2@pdx.edu>
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_contract_standards::non_fungible_token::{Token, TokenId};
+use near_contract_standards::non_fungible_token::{NonFungibleToken, Token, TokenId};
 use near_contract_standards::non_fungible_token::metadata::{NFTContractMetadata, TokenMetadata};
-use near_sdk::{env, near_bindgen, AccountId, Balance, PanicOnDefault, Promise, PromiseOrValue};
-use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
+use near_sdk::{env, near_bindgen, AccountId, PanicOnDefault, BorshStorageKey};
+use near_sdk::collections::{LazyOption};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
-    // Account ID of contract owner
-    pub owner_id: AccountId,
-    // Map of all token ID's currently owned for a given account
-    pub tokens_per_owner: LookupMap<AccountId, UnorderedSet<TokenId>>,
-    // Map tracking all Tokens by their TokenId
-    pub tokens_by_id: LookupMap<TokenId, Token>,
-    // Map tracking all Tokens' metadata by TokenId
-    pub token_metadata_by_id: UnorderedMap<TokenId, TokenMetadata>,
-    // Metadata for this contract
-    pub metadata: LazyOption<NFTContractMetadata>,
+    token_manager: NonFungibleToken,
+    metadata: LazyOption<NFTContractMetadata>,
 }
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    NonFungibleToken,
+    Metadata,
+    TokenMetadata,
+    Enumeration,
+    Approval,
+}
+
+#[near_bindgen]
+impl Contract {
+
+
+    /// Initialize the contract; can only be called once.
+    #[init]
+    pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
+        assert!(!env::state_exists(), "Contract has already been initialized.");
+        metadata.assert_valid();
+        Self {
+            token_manager: NonFungibleToken::new(
+                StorageKey::NonFungibleToken,
+                owner_id,
+                Some(StorageKey::TokenMetadata),
+                Some(StorageKey::Enumeration),
+                Some(StorageKey::Approval)
+            ),
+            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata))
+        }
+    }
+
+    /// Mint a new token of id `token_id` for new token owner `receiver_id`
+    #[payable]
+    pub fn nft_mint(
+        &mut self,
+        token_id: TokenId,
+        receiver_id: AccountId,
+        token_meta: TokenMetadata
+    ) -> Token {
+        self.token_manager
+            .internal_mint(token_id, receiver_id, Some(token_meta))
+    }
+
+}
+
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
     fn first_test() {
+        let test_lookup_map = LookupMap::new("test");
         println!("Woohoo! We testin'!")
     }
 }
